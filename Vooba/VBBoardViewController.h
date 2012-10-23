59f57943aaa40ff1bfb407d6f10f1335412d6b30
Ryan Wersal <ryan@ryanwersal.com>
2012-10-22 23:44:07 -0500

    A few silly basic additions to provide a base for the app

---
+++ Vooba/VBBoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBBoardViewController.h
+//  Vooba
+//
+//  Created by Ryan Wersal on 10/22/12.
+//  Copyright (c) 2012 Ryan Wersal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBBoardViewController : UIViewController
+
+@end
---
+++ Vooba/VBBoardViewController.m
@@ -0,0 +1,38 @@
+//
+//  VBBoardViewController.m
+//  Vooba
+//
+//  Created by Ryan Wersal on 10/22/12.
+//  Copyright (c) 2012 Ryan Wersal. All rights reserved.
+//
+
+#import "VBBoardViewController.h"
+
+@interface VBBoardViewController ()
+
+@end
+
+@implementation VBBoardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
